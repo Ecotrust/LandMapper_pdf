1729d79a56d04dd0fa40c20b8596694bea7ba22a
David Diaz <david.daniel.diaz@gmail.com>
2020-07-27 13:08:48 -0700

    Function to fill a PDF using pdfjinja

---
+++ src/fill_pdf.py
@@ -0,0 +1,30 @@
+from pdfjinja import PdfJinja
+import os
+
+
+def fill_pdf(pdf_template, fields_to_fill, outfile):
+    """Populates a PDF template with fields specified by user and writes the
+    filled PDF to disk. Field names should be defined in the pdf_template.
+
+    Parameters
+    ----------
+    pdf_template : str, path to file
+      path to the template PDF
+    fields_to_fill : dict
+      fields to populate and the values to populate them with
+    outfile : str, path to file
+      path to the PDF output file that will be generated
+
+    Raises
+    ------
+    FileNotFoundError
+      if output file is not generated
+    """
+    template = PdfJinja(pdf_template)
+    filled = template(fields_to_fill)
+    filled.write(open(outfile, 'wb'))
+
+    if os.path.exists(outfile):
+        return outfile
+    else:
+        raise FileNotFoundError('Failed to produce output file.')
